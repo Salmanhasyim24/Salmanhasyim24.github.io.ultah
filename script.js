4c1f1ebf2a5a9ea9505b0e7143f559da2960cd07
Salman Hasyim Adz Dzikri <98438105+Salmanhasyim24@users.noreply.github.com>
2024-04-03 22:44:03 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+// Initialize the carousel
+var myCarousel = document.querySelector('#slider');
+var carousel = new bootstrap.Carousel(myCarousel, {
+  interval: false // Disable automatic sliding
+});
